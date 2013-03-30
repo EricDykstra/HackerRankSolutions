f7e8ea8449e347f8b300662fe81e41e9cccbea77
eric@ericdykstra.me <eric@sway.(none)>
2013-03-29 17:28:50 -0700

    FizzBuzz and Pairs challenges

---
+++ Pairs/n-in-k.rb
@@ -0,0 +1,19 @@
+file = File.open("n-in-k.txt", "rb")
+contents = file.read
+x = contents.split.map(&:to_i)
+numbers = x[2..-1].sort
+diff = x[1].to_i
+count = 0
+numbers.each_with_index do |x, i|
+  1.upto(diff) do |num|
+    next if numbers[i+num] == nil
+    if numbers[i+num] - x == diff
+      count +=1 
+      next
+    end
+  end
+end
+p count
+
+file = STDIN.readlines
+contents = file.join(" ")
